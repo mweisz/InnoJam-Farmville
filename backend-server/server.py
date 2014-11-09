bd5e7cd6f1702ab868a8b08a1ac6ca9ba32876f2
Leo Kotschenreuther <leo.kotschenreuther@student.hpi.uni-potsdam.de>
2014-11-09 18:04:30 +0100

    Added missing routes (some might not work as expected yet)

--- backend-server/server.py
+++ backend-server/server.py
@@ -77,12 +77,16 @@ def getDataFromDb(sql, params):
 		result.append(row)
 	return result
 
+@app.route('/innojam/fertilizeField', methods=['POST'])
+@crossdomain("*", headers='Origin, X-Requested-With, Content-Type, Accept')
+def fertilizeField():
+	return "FertilizeField"
+
 @app.route('/innojam/fields')
 @crossdomain("*", headers='Origin, X-Requested-With, Content-Type, Accept')
 def getAllFields():
 	sql = "SELECT ID, X, Y FROM FARMVILLE.FIELD"
 	result = getDataFromDb(sql, None)
-	
 	return Response(json.dumps(result),  mimetype='application/json')
 
 @app.route('/innojam/allAvailablePlants')
@@ -90,8 +94,16 @@ def getAllFields():
 def getAvailablePlants():
 	sql = "SELECT ID, NAME FROM FARMVILLE.PLANTS"
 	result = getDataFromDb(sql, None)
-	
 	return Response(json.dumps(result),  mimetype='application/json')
+
+@app.route('/innojam/brightness')
+@crossdomain("*", headers='Origin, X-Requested-With, Content-Type, Accept')
+def getBrightness():
+	# fieldID = request.args.get('fieldId', '')
+	# sql = "SELECT ID, FIELDID, PLANT, EVENT, TIME, VALUE FROM FARMVILLE.PLANTS WHERE FIELDID = ? AND EVENT = 'Light'"
+	# result = getDataFromDb(sql, fieldID)
+	# return Response(json.dumps(result),  mimetype='application/json')
+	return "Brightness"
 
 @app.route('/innojam/field')
 @crossdomain("*", headers='Origin, X-Requested-With, Content-Type, Accept')
@@ -99,8 +111,16 @@ def getField():
 	fieldID = request.args.get('fieldId', '')
 	sql = "SELECT ID, X, Y FROM FARMVILLE.FIELD WHERE ID = ?"
 	result = getDataFromDb(sql, (fieldID))
-	
 	return Response(json.dumps(result[0]),  mimetype='application/json')
+
+@app.route('/innojam/photo')
+@crossdomain("*", headers='Origin, X-Requested-With, Content-Type, Accept')
+def getPhoto():
+	# fieldID = request.args.get('fieldId', '')
+	# sql = "SELECT ID, FIELDID, PLANT, EVENT, TIME, VALUE FROM FARMVILLE.PLANTS WHERE FIELDID = ? AND EVENT = 'Light'"
+	# result = getDataFromDb(sql, fieldID)
+	# return Response(json.dumps(result),  mimetype='application/json')
+	return "Photo"
 
 @app.route('/innojam/plantInField')
 @crossdomain("*", headers='Origin, X-Requested-With, Content-Type, Accept')
@@ -108,8 +128,43 @@ def getPlantInField():
 	fieldID = request.args.get('fieldId', '')
 	sql = "SELECT FIELDID, PLANT FROM FARMVILLE.EVENTS WHERE FIELDID = ?"
 	result = getDataFromDb(sql, (fieldID))
-	
 	return Response(json.dumps(result[0]),  mimetype='application/json')
+
+@app.route('/innojam/temperature')
+@crossdomain("*", headers='Origin, X-Requested-With, Content-Type, Accept')
+def getTemperature():
+	fieldID = request.args.get('fieldId', '')
+	sql = "SELECT ID, FIELDID, PLANT, EVENT, TIME, VALUE FROM FARMVILLE.PLANTS WHERE FIELDID = ? AND EVENT = 'Temperature'"
+	result = getDataFromDb(sql, fieldID)
+	return Response(json.dumps(result),  mimetype='application/json')
+
+@app.route('/innojam/wettness')
+@crossdomain("*", headers='Origin, X-Requested-With, Content-Type, Accept')
+def getWettness():
+	fieldID = request.args.get('fieldId', '')
+	sql = "SELECT ID, FIELDID, PLANT, EVENT, TIME, VALUE FROM FARMVILLE.PLANTS WHERE FIELDID = ? AND EVENT = 'Water'"
+	result = getDataFromDb(sql, fieldID)
+	return Response(json.dumps(result),  mimetype='application/json')
+
+@app.route('/innojam/harvestField', methods=['POST'])
+@crossdomain("*", headers='Origin, X-Requested-With, Content-Type, Accept')
+def harvestField():
+	return "harvestField"
+
+@app.route('/innojam/turnLightsOff', methods=['POST'])
+@crossdomain("*", headers='Origin, X-Requested-With, Content-Type, Accept')
+def turnLightsOff():
+	return "TurnLightsOff"
+
+@app.route('/innojam/turnLightsOn', methods=['POST'])
+@crossdomain("*", headers='Origin, X-Requested-With, Content-Type, Accept')
+def turnLightsOn():
+	return "TurnLightsOn"
+
+@app.route('/innojam/waterField', methods=['POST'])
+@crossdomain("*", headers='Origin, X-Requested-With, Content-Type, Accept')
+def waterField():
+	return "Watered Field"
 
 
 
