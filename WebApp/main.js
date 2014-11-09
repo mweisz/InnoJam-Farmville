2e403360b798a4cfbce932c999a7a7f40dd555d5
Michael Weisz <mail@michael-weisz.de>
2014-11-09 21:29:53 +0100

    Main.js updated

--- WebApp/main.js
+++ WebApp/main.js
@@ -88,3 +88,46 @@ function showPlantSelector() {
 function hidePlantSelector() {
 	plantSelectorContainer.style.top = "-500px";
 }
+
+
+function updateCharts(){
+	var chart = document.getElementById("chart_container");
+	chart.highcharts({
+        title: {
+            text: 'Monthly Average Temperature',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com',
+            x: -20
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }
+        ]
+    });
+}
